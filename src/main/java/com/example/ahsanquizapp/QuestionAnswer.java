bfdbd885ea492849f43e6d5086b73b5b9c54d7ec
shahg023 <139157081+shahg023@users.noreply.github.com>
2023-09-06 11:28:14 +0500

    Add files via upload

---
+++ src/main/java/com/example/ahsanquizapp/QuestionAnswer.java
@@ -0,0 +1,35 @@
+package com.example.ahsanquizapp;
+
+public class QuestionAnswer {
+
+    public static String[] question ={
+            "Which company owns the android?",
+            "Which one is not the programming language?",
+            "What is the name of this app creater?",
+            "Android Studio Is Launched in?",
+            "Who is the Ex Prime minister of Pakistan?",
+            "The Population of Pakistan Is?",
+            "Which is a reserved word in the Java programming language?",
+    };
+
+    public static String[][] choices = {
+            {"Google","Apple","Nokia","Samsung"},
+            {"Java","Kotlin","Notepad","Python"},
+            {"Facebook","Whatsapp","Ahsan","Youtube"},
+            {"2013","2010","2006","2000"},
+            {"Nawaz Shareef","Imran Khan","Bilawal Bhutto","Maryam Nawaz"},
+            {"20 crore","28 crore","24 crore","22 crore"},
+            {"method","native","subclasses","reference","array"}
+    };
+
+    public static String[] correctAnswers = {
+            "Google",
+            "Notepad",
+            "Ahsan",
+            "2013",
+            "Imran Khan",
+            "24 crore",
+            "native"
+    };
+
+}
